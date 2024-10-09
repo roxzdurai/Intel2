eb93c96ef03237f178fa785232e20770c86f690e
RAJADURAI S <130030355+roxzdurai@users.noreply.github.com>
2024-10-09 20:07:15 +0530

    Update Navbar.tsx

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import { FiBell, FiShare2, FiSearch } from 'react-icons/fi';  
+import { FiBell, FiShare2, FiSearch } from 'react-icons/fi';
 
 const Navbar: React.FC = () => {
   const [searchQuery, setSearchQuery] = useState('');
@@ -16,9 +16,12 @@ const Navbar: React.FC = () => {
   return (
     <header className="bg-white py-2 px-6 w-full flex justify-between items-center shadow-md">
       {/* Search Bar */}
-      <form onSubmit={handleSubmit} className="flex items-center bg-gray-100 rounded-full px-4 py-2 w-1/3 border border-gray-300">
+      <form
+        onSubmit={handleSubmit}
+        className="flex items-center bg-gray-100 rounded-full px-4 py-2 w-full md:w-1/3 ml-4 md:ml-0 border border-gray-300"
+      >
         {/* Search Icon */}
-        <FiSearch className="text-gray-500 mr-2 w-4 h-4" />  {/* Add Search Icon */}
+        <FiSearch className="text-gray-500 mr-2 w-4 h-4" /> {/* Add Search Icon */}
         <input
           type="text"
           value={searchQuery}
@@ -26,7 +29,7 @@ const Navbar: React.FC = () => {
           placeholder="Search Keyword"
           className="bg-transparent w-full focus:outline-none text-gray-700 placeholder-gray-500"
         />
-        <span className="text-gray-500">/</span> {/* Slash (/) for search */}
+        <span className="text-gray-500"></span>
       </form>
 
       {/* Right Icons */}
